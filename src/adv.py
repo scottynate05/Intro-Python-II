d16244243e132519efa9bf93097bcba654a6b4b3
Nathan Brown <scottynate05@yahoo.com>
2020-09-09 18:53:52 -0700

    forgot to make a push last night

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -38,6 +39,44 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+
+player1 = Player('Nate', current_room = room['outside'])
+print(f"\n{player1.name}'s starting room: {player1.current_room.name}")
+print(f"{player1.current_room.description}")
+
+while True:
+    movement = {
+        'n': lambda x: x.n_to,
+        's': lambda x: x.s_to,
+        'e': lambda x: x.e_to,
+        'w': lambda x: x.w_to,
+    }
+    user_input = input('\nWhere would you like to go? ')
+    if user_input == 'q':
+        print('\nThank you for playing!')
+        exit()
+    try:
+        if not user_input:
+            continue
+        else:
+            user_input = user_input[0].lower()
+        if user_input in movement.keys():
+            target = movement[user_input](player1.current_room)
+            if target:
+                player1.current_room = target
+                print(f'\n{player1.name} enters the {player1.current_room.name}')
+                print(f'{player1.current_room.description}')
+            else:
+                exit()
+        else:
+            print(f'\nYou cannot go that way, "{user_input}" is not a valid '
+                f'direction.\n[n] Move North   [w] Move West   [s] Move South   [e] Move East   [q] Quit')
+            print(f'\n{player1.name} is currently at the {player1.current_room.name}')
+            print(f'{player1.current_room.description}')
+    except:
+        print("\nMovement not allowed")
+        print(f'\n{player1.name} is currently at the {player1.current_room.name}')
+        print(f'{player1.current_room.description}')
 
 # Write a loop that:
 #
